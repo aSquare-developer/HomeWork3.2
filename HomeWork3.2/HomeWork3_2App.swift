7d87e27b3318ec1ee946b394ee65766eeed2f8df
aSquare <artur.anissimov@gmail.com>
2022-01-25 20:03:10 +0200

    Initial Commit

---
+++ HomeWork3.2/HomeWork3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork3_2App.swift
+//  HomeWork3.2
+//
+//  Created by Artur Anissimov on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
