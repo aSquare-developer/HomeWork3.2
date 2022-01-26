2d7fc05d36c36de944a7fdcec1bca9a9cbe0f23a
aSquare <artur.anissimov@gmail.com>
2022-01-26 21:48:23 +0200

    Code Refactoring Create TextView

---
+++ HomeWork3.2/TextView.swift
@@ -0,0 +1,28 @@
+//
+//  TextView.swift
+//  HomeWork3.2
+//
+//  Created by Artur Anissimov on 26.01.2022.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    
+    let value: Double
+    
+    var body: some View {
+        Text("\(lround(value))")
+            .frame(width: 35, alignment: .leading)
+            .foregroundColor(.white)
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.gray
+            TextView(value: 128)
+        }
+    }
+}
